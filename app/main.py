8a17cb4cb1202a4b017640c884f7a789f22500b6
Dev Olusegun <12515139o@gmail.com>
2025-04-08 10:21:02 +0100

    CORS policy fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Multibooker API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://multibooker.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
