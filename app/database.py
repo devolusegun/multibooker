372af3b91658dcb7daae76de415beacdeb8395b5
Dev Olusegun <12515139o@gmail.com>
2025-04-12 19:07:07 +0100

    db routing fix

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env
 
 # DATABASE_URL format: "postgresql://user:password@localhost/dbname"
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://native:SUOMTHu6QPgwdeyfaz49oSyoSmG6AZZG@dpg-cvt9b9re5dus73a4s540-a.oregon-postgres.render.com/multibookdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
